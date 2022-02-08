bfee1fa022e45416193c5f8032f79a15ca92f34a
Nick Hayden <hayden.33@wright.edu>
2022-02-07 23:33:05 -0500

    Finished Conditionals

--- JavaScript Basics/Boolean logic/12_Boolean_Logic/Test.js
+++ JavaScript Basics/Boolean logic/12_Boolean_Logic/Test.js
@@ -1,2 +1,12 @@
-console.log("Hello from the console log!!!");
-console.error("ERROR!!!!!");
+// console.log("Hello from the console log!!!");
+// console.error("ERROR!!!!!");
+// console.warn("Warning!");
+// console.info("Clearing data!");
+
+const password = prompt("Please enter a new password");
+
+if(password.length >= 6 && password.indexOf(' ') === -1){
+   console.log("VALID PASSWORD!");
+} else {
+    console.log("INCORRECT FORMAT FOR PASSWORD!");
+}
